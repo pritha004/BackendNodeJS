9cf9db8abbd5dd5a301be3335a63abfc32752813
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-07-04 18:04:00 +0000

    Created an express application

--- 03backend/src/index.js
+++ 03backend/src/index.js
@@ -1,12 +1,27 @@
 // APPROACH 2: Recommended- Create connection func in seperate db folder
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+
+    app.on("errror", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(`MONGODB connection failed!! ${err}`);
+})
 
 
 /*
