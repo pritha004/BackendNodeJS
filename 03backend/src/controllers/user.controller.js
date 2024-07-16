40f795a5ef4626104c56cdd28ad640caf8dbb502
pritha004 <prithamondal004@gmail.com>
2024-07-16 12:28:47 +0530

    Added login and logout functionality (accessToken, refreshToken, custom middleware).

--- 03backend/src/controllers/user.controller.js
+++ 03backend/src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens=async(userId)=>{
+    try {
+        const user= await User.findById(userId);
+        const accessToken=user.generateAccessToken();
+        const refreshToken=user.generateRefreshToken();
+
+        user.refreshToken=refreshToken;
+        await user.save({validateBeforeSave:false});
+
+        return {accessToken,refreshToken};
+
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating refresh and access token.")
+    }
+}
+
 const registerUser=asyncHandler(async(req,res)=>{
     // get user details from frontend (here postman)
     // validation - not empty
@@ -76,4 +92,82 @@ const registerUser=asyncHandler(async(req,res)=>{
 
 })
 
-export {registerUser}
+const loginUser=asyncHandler(async(req,res)=>{
+    // req body -> data
+    // username or email required
+    // find the user
+    // if user present -> password check; if user not present -> error
+    // if password valid -> generate access and refresh token; else throw error
+    // get the updated user with refresh token -> send cookie
+
+    const {username,email,password}=req.body;
+
+    if(!username || !email){
+        throw new ApiError(400,"username or email is required")
+    }
+
+    const user= await User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(!user){
+        throw new ApiError(404,"User does not exist.")
+    }
+
+    const isPasswordValid= await user.isPasswordCorrect(password);
+    if(!isPasswordValid){
+        throw new ApiError(401,"Invalid user credentials.")
+    }
+
+    const {accessToken,refreshToken}= await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+    //cookie options for security: will not be modifiable at frontend
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res.status(200)
+              .cookie("accessToken",accessToken,options)
+              .cookie("refreshToken",refreshToken,options)
+              .json(
+                new ApiResponse(200,{
+                    user:loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User logged in successfully."
+                )
+              )
+})
+
+const logoutUser=asyncHandler(async(req,res)=>{
+    // remove accessToken and refreshToken from cookies
+    // reset refreshToken
+
+    await User.findByIdAndUpdate(req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+    return res.status(200)
+              .clearCookie("accessToken",options)
+              .clearCookie("refreshToken",options)
+              .json(new ApiResponse200,{},"User logged out successfully.")
+})
+
+export {registerUser,
+    loginUser,
+    logoutUser
+}
--- 03backend/src/models/user.model.js
+++ 03backend/src/models/user.model.js
@@ -60,7 +60,7 @@ userSchema.methods.isPasswordCorrect=async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken= function(password){
+userSchema.methods.generateAccessToken= function(){
     return jwt.sign(
         {
             _id:this._id,
@@ -75,7 +75,7 @@ userSchema.methods.generateAccessToken= function(password){
     )
 }
 
-userSchema.methods.generateRefreshToken= function(password){
+userSchema.methods.generateRefreshToken= function(){
     return jwt.sign(
         {
             _id:this._id,
