f8646a9ecc89b9756d1a82d28d1f5b33a878f58c
pritha004 <prithamondal004@gmail.com>
2024-07-16 15:14:11 +0530

    Typo fixes.

--- 03backend/src/controllers/user.controller.js
+++ 03backend/src/controllers/user.controller.js
@@ -102,7 +102,7 @@ const loginUser=asyncHandler(async(req,res)=>{
 
     const {username,email,password}=req.body;
 
-    if(!username || !email){
+    if(!username && !email){
         throw new ApiError(400,"username or email is required")
     }
 
@@ -164,7 +164,7 @@ const logoutUser=asyncHandler(async(req,res)=>{
     return res.status(200)
               .clearCookie("accessToken",options)
               .clearCookie("refreshToken",options)
-              .json(new ApiResponse200,{},"User logged out successfully.")
+              .json(new ApiResponse(200,{},"User logged out successfully."))
 })
 
 export {registerUser,
